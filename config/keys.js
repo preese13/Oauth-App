629d354a35bb2dbe7f78281873ee1cdf97ae94b4
Patrick Reese <preese@employeenavigator.com>
2018-09-19 14:32:32 -0400

    helloworld

---
+++ config/keys.js
@@ -0,0 +1,15 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: "6892006663-g783nnkov3c1t8m1mop9ak4rsq0h5nig.apps.googleusercontent.com",
+        clientSecret: "NBjgjjtk7pkadQGlWs4Bh9mu",
+        callbackURL: "auth/google/redirect"
+    },
+    mongodb: {
+        dbURI: 'mongodb://test:test13@ds163382.mlab.com:63382/oauth-test'
+    },
+    session: {
+        cookieKey: 'cookieKey'
+    }
+};
